c6f49114d08d70d211f54acfdcb8b226a7839ced
Digital Revolution <stephen.vandelinder@gmail.com>
2016-03-03 21:29:54 -0700

    Solved scope bugs and created new template for new user creation. Journal entry features to follow soon.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var dietApp = angular.module("dietApp", ["firebase"])
 
-	.controller("MenuController",  function($scope, $firebaseArray){
+	.controller("MainController",  function($scope, $firebaseArray){
 
 		// Access the Database
 		var menu = new Firebase("https://dietjournalapp.firebaseio.com/Menu");
@@ -15,38 +15,66 @@ var dietApp = angular.module("dietApp", ["firebase"])
 		    Protein: $scope.newItemPro,
 		    Calories: $scope.newItemCal
 			});
-			clearForm();
+			clearUserMenu();
 		};
 
-		var clearForm = function(){
+		var clearUserMenu = function(){
 			$scope.newItemName = '';
 			$scope.newItemPro = '';
 			$scope.newItemCal = '';
 			$scope.newItemFat = '';
-		}; 
+		};
+
 
 		$scope.showMenu = function(){
 			menuFormHidden = !menuFormHidden;
-		}
+		};
 
-	})
-
-	.controller("UserController", function($scope, $firebaseArray){
-
+		// USER FUNCTIONS
 		var users = new Firebase("https://dietjournalapp.firebaseio.com/Users"); 
 		$scope.users = $firebaseArray(users);
 
 		$scope.addUser = function (){
 			$scope.users.$add({
 				FirstName: $scope.firstName,
-				LastName: $scope.lastName
+				LastName: $scope.lastName, 
+				CurrentWeight: $scope.currentWeight,
+				TargetWeight: $scope.targetWeight,
+				TargetProtein: $scope.targetProtein,
+				TargetFat: $scope.targetFat,
+				TargetCalories: $scope.targetCalories, 
+				Journal: {}
 			});
 			clearForm(); 
 		};
 
 		var clearForm = function(){
 			$scope.firstName = "";
-			$scope.lastName = ""; 
+			$scope.lastName = "";
+			$scope.currentWeight = "";
+			$scope.targetWeight = "";
+			$scope.targetProtein = ""; 
+			$scope.targetFat = ""; 
+			$scope.targetCalories = ""; 
+		};
+
+		// JOURNAL FUNCTIONS
+
+		var journal = new Firebase("https://dietjournalapp.firebaseio.com/Journal"); 
+		$scope.journal = $firebaseArray(journal); 
+
+		$scope.addJournalEntry = function(){
+			console.log($scope.this);
+			// $scope.journal.$add({
+			// 	//User: $scope.newEntry,
+			// 	Food: $scope.newEntry, 
+			// 	Time: Date()
+			// });
+		};
+
+		$scope.displayItemStats = function(index){
+			console.log(index);
 		};
 
 	})
+
